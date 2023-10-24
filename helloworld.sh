2f07bb2dda9a5bf99c2ffda2022e8a0eee491231
mj roberts <michael.j.roberts33@gmail.com>
2023-10-24 16:08:31 -0500

    creating first .sh file

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Michael Roberts 
+# Date of latest revision:      10/24/2023
+# Purpose:                      to create a file through ssh
+
+# Declaration of variables
+first_variable="I am working perfectly"
+# Declaration of functions
+
+
+# Main
+
+echo "hello world"
+echo $first_variable
+
+# End
