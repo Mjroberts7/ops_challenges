12be80e4e99486c08b43e091734099ce965e46c0
mj roberts <michael.j.roberts33@gmail.com>
2023-10-25 16:32:34 -0500

    login history function call x3

---
+++ OPs201_function1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script Name:                  OPs201_function1.sh
+# Author:                       Michael Roberts 
+# Date of latest revision:      10/25/2023
+# Purpose:                      to create a function and print x3
+# Execution:			        bash OPs201_function1.sh or ./OPs201_function1.sh chmod -x OPs201_function1.sh
+
+# Declaration of variables
+
+# Declaration of functions
+
+get_login_history() {
+    login_history=$(grep -v "sshd" /var/log/auth.log)
+    echo login_history
+    echo "This is the login history."
+}
+
+# Main
+
+get_login_history
+get_login_history
+get_login_history
+
+
+# End
