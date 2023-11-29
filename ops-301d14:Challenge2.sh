b1bbba5ba3bbea7b5960d14a7e16dfde2f8665a2
mj roberts <michael.j.roberts33@gmail.com>
2023-11-29 17:13:46 -0600

    refined the code to create temp dir/file and add comments

--- ops-301d14:Challenge2.sh
+++ ops-301d14:Challenge2.sh
@@ -7,11 +7,17 @@
 # Execution:			        bash ops-301d14:Challenge2.sh or ./ops-301d14:Challenge2.sh chmod -x ops-301d14:Challenge2.sh
 # Documentation                 Chap-GPT and info was used here https://chat.openai.com/share/0b2fa5cf-0ed4-4eca-bd4a-9bbc00832560
 
-echo "what is the directory path: " && read name
+# Before this command i like to create a dir and name it testFolder and file within the folder
+# named testFile.txt and then ls -l to show the permissions of the files. The -r interprets / as literals
 
-echo "what permissions numbers (only 000-777) would you like to mod: " && read numbers
+echo -n "what is the directory path: " && read -r name
+
+echo -n "what permissions numbers (only 000-777) would you like to mod: " && read numbers
 
 cd $name 
-chmod $numbers
+chmod $numbers *
+# The star is a 'Wildcard' which selects all the files within the folder
 
-ls -al
+ls -l
+
+# after this is done you can rm -rf testFolder to remove it
