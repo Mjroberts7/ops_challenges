38bc079aba344c059f2b27f60ce6a514615ba189
mj roberts <michael.j.roberts33@gmail.com>
2023-12-06 12:56:51 -0600

    started on challenge 7

---
+++ ops-301d14_Challenge7.py
@@ -0,0 +1,11 @@
+# Import libraries
+
+import os
+
+# Script Name:                  ops-301d14_Challenge7.py
+# Author:                       Michael Roberts 
+# Date of latest revision:      12/06/2023
+# Purpose:                      use python to 
+# Execution:			        python3 ops-301d14_Challenge6.py 
+# Documentation                 
+
