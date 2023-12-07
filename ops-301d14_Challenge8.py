6005873a89c37931f91d0aa6d3764de65a934008
mj roberts <michael.j.roberts33@gmail.com>
2023-12-07 17:14:59 -0600

    finished challenge 8

--- ops-301d14_Challenge8.py
+++ ops-301d14_Challenge8.py
@@ -1,5 +1,5 @@
 # Import libraries
-
+import time
 
 # Script Name:                  ops-301d14_Challenge8.py
 # Author:                       Michael Roberts 
@@ -8,49 +8,62 @@
 # Execution:			        python3 ops-301d14_Challenge8.py 
 # Documentation                 
 
-question = input('would you like to compare a int or str? ')
-
-
-while question == 'int':
-
-    inputa = int(input('what is the first number? '))
-    inputb = int(input('what is the second number? '))
-
-
-    if inputa < inputb: 
-        print(f'{inputa} is less than {inputb}')
-        if inputa < 5:
-            print('those are rookie numbers\n need to pump them up')
-        elif inputa <= inputb:
-            print(f'{inputa} is less than or equal to {inputb}')
-    elif inputa > inputb:
-        print(f'{inputa} is greater than {inputb}')
-        if inputa >= inputb:
-            print(f'{inputa} is greater than or equal to {inputb}')
+while True: 
+    question = input('would you like to compare a int or str? ')
+    if question == 'int' or question == 'str':
+        answer = 1
     else:
-        print('invalid input')
+        print(f'{question} is not an answer, goodbye')
         break
-done
 
-while question == 'str':
-    
-    inputa = str(input('what is the first word? '))
-    inputb = str(input('what is the second word? '))
+    while answer == 1:
+        try:
+            inputa = int(input('what is the first number? '))
+            print('...')
+            inputb = int(input('what is the second number? '))
+            print('...')
+            time.sleep(1)
+        except ValueError:
+            print('that dont work')
+            break 
 
-    if inputa == inputb:
-        print(f'{inputa} and {inputb} are the same')
-    
-    if inputa != inputb:
-        print(f'{inputa} and {inputb} are not the same')
-        continuation = input('do you want to keep going? ')
-        if continuation == 'N' or continuation == 'No' or continuation =='no' or continuation == 'n':
-            break
+        if inputa <= inputb:
+            print(f'{inputa} is less than or equal to {inputb}')
+            if inputa < inputb: 
+                print(f'{inputa} is less than {inputb}')
+                if inputa < 5:
+                    print('those are rookie numbers\n need to pump them up')
+        elif inputa >= inputb:
+            print(f'{inputa} is greater than or equal to {inputb}')
+            if inputa > inputb:
+                print(f'{inputa} is greater than {inputb}')
         else:
-            continue
+            print('invalid input')
+            break
+    
 
-    if inputa == 'whiskey' or inputb == 'wine':
-        print('Classy')
-    elif inputa == 'beer' and inputb == 'beer':
-        print('Cheers!')    
-done
+    while answer == 1:
+    
+        inputa = str(input('what is the first word? '))
+        print('...')
+        inputb = str(input('what is the second word? '))
+        print('...')
+        time.sleep(1)
+
         
+        if inputa == inputb:
+            print(f'{inputa} and {inputb} are the same')
+            if inputa == 'beer' and inputb == 'beer':
+                print('Cheers!')    
+            
+        if inputa == 'whiskey' or inputb == 'wine':
+            print('Classy')
+
+        if inputa != inputb:
+            print(f'{inputa} and {inputb} are not the same')
+        
+        wannacontinue = input('wanna continue? ')
+        if wannacontinue == 'no' or wannacontinue == 'NO' or wannacontinue == 'N' or wannacontinue == 'No' or wannacontinue == 'n': 
+            break       
+    
+ 
