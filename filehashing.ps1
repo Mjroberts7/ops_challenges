640887bab5bbbaafdb0ba3d76d00f72a159c3613
mj roberts <michael.j.roberts33@gmail.com>
2024-01-15 12:56:39 -0600

    created a powershell file to get hashes for fun

---
+++ filehashing.ps1
@@ -0,0 +1,16 @@
+
+
+
+# Script Name:                  filehashing.ps1
+# Author:                       Michael Roberts 
+# Date of latest revision:      01/15/2024
+# Purpose:                      powershell script writes a string to a new temp file, gets the hash and then deletes the file simulating the hash of a string 
+# Execution:			        powershell filehashing.ps1
+# Documentation                 Microsoft Documentation https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-filehash?view=powershell-7.4
+
+
+$tempfile = NewTemporaryFile
+hashstring = Read-Host "What string would you like to get the hash of?"
+echo "$hashstring" > $tempfile
+Get-FileHash $tempfile | Format-List 
+rm $tempfile
