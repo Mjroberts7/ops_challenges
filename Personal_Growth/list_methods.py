fee3e5aee7c63adeab801801ffc4333c409c4311
mj roberts <michael.j.roberts33@gmail.com>
2024-04-16 13:06:41 -0500

    altering a list with inputed number of methods

---
+++ Personal_Growth/list_methods.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Author:           Michael roberts
+# Date:             04/16/2024
+
+class ListOperations:
+        def __init__(self):
+            self.lst = [] 
+    
+        def insert(self, idx, value):
+            self.lst.insert(idx, value)
+        
+        def remove(self, value):
+            self.lst.remove(value)
+        
+        def append(self, value):
+            self.lst.append(value)
+        
+        def sort(self):
+            self.lst.sort()
+        
+        def pop(self):
+            self.lst.pop()
+        
+        def reverse(self):
+            self.lst.reverse()
+        
+        def execute_command(self, command, *args):
+            if hasattr(self, command):
+                getattr(self, command)(*args)
+            else:
+                print("command not recognized")
+        
+# For each time print is inputed, the outputted list will be printed on a new line.
+if __name__ == '__main__':
+    n = int(input())
+    
+    operations = ListOperations()
+    
+    for line in range(n):
+        line = str(input()).strip()
+        line = line.split()
+        command = line[0]
+        args = list(map(int, line[1:]))
+        if command == 'print':
+            print(operations.lst)
+        else:
+            operations.execute_command(command, *args)
+    
+    # These are the commands that you can put within the input after you input an int that tells you how many commands to run
+    '''
+    insert i e: Insert integer at position e
+    print: Print the list.
+    remove e: Delete the first occurrence of integer e
+    append e: Insert integer at the end of the list.
+    sort: Sort the list.
+    pop: Pop the last element from the list.
+    reverse: Reverse the list.
+    '''
